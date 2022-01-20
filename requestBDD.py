83d5f3562bd71ee26f1b243e1a206995001ca915
Baptiste Prévost <baptiste.prevost@telecom-paris.fr>
2022-01-20 10:41:11 +0000

    Correct BDD access for learnClf

--- requestBDD.py
+++ requestBDD.py
@@ -1,4 +1,5 @@
 import sys
+import os
 package_directory = os.path.dirname(os.path.abspath(__file__))
 sys.path.append('/var/fantasiapp/batiUni/middle/')
 from bdd import executeRequest
@@ -6,6 +7,6 @@ from bdd import executeRequest
 
 def getEmbeddings():
     fields = ', '.join([f"e.dim{i+1}" for i in range(50)])
-    query = f"SELECT j.name, {fields} FROM embeddings AS e JOIN (SELECT d.id, l.name FROM documents AS d JOIN labels ON d.label_id = l.id) AS j ON j.jid = e.doc_id"
+    query = f"SELECT j.name, {fields} FROM embeddings AS e JOIN (SELECT d.id, l.name FROM documents AS d JOIN labels AS l ON d.label_id = l.id) AS j ON j.id = e.doc_id"
     results = executeRequest(query, True)
     return ([row[0] for row in results], [row[1:] for row in results])
